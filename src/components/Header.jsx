882218911a0ae9a1451a5af05c58278ab787e53e
My Roman <klaytfrg@gmail.com>
2023-02-16 10:36:06 +0200

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header =()=> {
+
+    return (
+    <header className='header'>
+       <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6fj0xguiKdBvzheum0vYrddRwRJ_3l3uqdd9VLpdQ2emVjz7VtRnFK8a2JaUUACGQfbc&usqp=CAU' alt='logo'/>
+    </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Profile =()=> {
+
+    return (
+        <div className='content'>
+        <div>
+        <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' alt='im' />
+        </div>
+        <div>
+        <img src='https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg' alt='im' />
+        ava + discription
+        </div>
+      <div>
+      My posts
+      <div>
+        new post
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+          post 2
+        </div>
+        </div>
+      </div>
+      </div>      
+      </div>
+    )
+}
+
+export default Profile;
