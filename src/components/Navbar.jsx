131f7fcd10b0fbc2760edd65e5dcea117ba3ba95
My Roman <klaytfrg@gmail.com>
2023-02-22 16:50:07 +0200

    add class Active

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import x from './navbar.module.css';
 const Navbar =()=> {
     return (
         <nav className={x.nav}>
-        <div className={x.item}>
+        <div className= {`${x.item} ${x.active}`}>
           <a href='#'>Profile</a>
         </div>
         <div className={x.item}>
